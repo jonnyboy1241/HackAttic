58649185b0506bb4b11e155cda9d5da976a918c3
Jonathan Marek <jonathan.marek@mavs.uta.edu>
2019-08-10 18:51:18 -0500

    Download .zip file successfully

--- Brute_Force_ZIP/bruteForceZIP.py
+++ Brute_Force_ZIP/bruteForceZIP.py
@@ -2,7 +2,20 @@
 
 import requests
 
+# Send request to get the JSON
 r = requests.get('https://hackattic.com/challenges/brute_force_zip/problem?access_token=')
-
 get_json = r.json()
+
 print(get_json)
+
+# Get the .zip file
+r = requests.get(get_json['zip_url'])
+filename = "./package.zip"
+
+# Write to the .zip file
+file = open(filename, 'wb')
+for chunk in r.iter_content(100000):
+    file.write(chunk)
+file.close()
+
+# Now theres a file named package.zip in the current directory
